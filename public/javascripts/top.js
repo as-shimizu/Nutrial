7b0dbd64f4062ddcaa7d8ba97248b1eea8ba9d7f
as-shimizu <asyk@User-no-MacBook-Air.local>
2018-07-08 15:59:00 +0900

    view change month to day

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -1,10 +1,11 @@
 function init() {
   $('#calendar').fullCalendar({
     header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month agendaWeek agendaDay'
+      left: 'today',
+      center: 'prev title next',
+      right:'month agendaDay'
     },
+//    titleFormat: 'MMMM YYYY',
     businessHours: {
       dow: [ 1, 2, 3, 4, 5], // Monday - Friday
     },
@@ -16,21 +17,77 @@ function init() {
     defaultView: 'month',
     timeFormat: 'H(:mm)',
     buttonText: {
-      prev:     ' < ',
-      next:     ' > ',
-      prevYear: ' << ',
-      nextYear: ' >> ',
       today:    '今日',
       month:    '月',
       week:     '週',
       day:      '日'
     },
-    titleFormat: 'YYYY年 MMMM',
     monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
     monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
     dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
     dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
     selectable: true,
-    allDayText: "終日"
+    allDayText: "終日",
+    views: {
+      month: {
+        titleFormat: "YYYY年 MMMM",
+      },
+      day: {
+        titleFormat: "YYYY年 MMMM D日 [(]ddd[)] "
+      },
+    }
   });
+
+  //customeis
+  var switchMonth = document.createElement("input");
+  var switchDay = document.createElement("input");
+  switchMonth.type = "radio";
+  switchDay.type = "radio";
+  switchMonth.name = "switchMonthDay";
+  switchDay.name = "switchMonthDay";
+  switchMonth.id = "switchMonth";
+  switchDay.id = "switchDay";
+  switchMonth.value = "month";
+  switchDay.value = "day";
+  switchMonth.setAttribute("checked","");
+
+  var labelMonth = document.createElement("label");
+  var labelDay = document.createElement("label");
+  labelMonth.for = "switchMonth";
+  labelDay.for = "switchDay";
+  labelMonth.innerText = "月";
+  labelDay.innerText = "日";
+
+  var fcRight = document.getElementsByClassName("fc-right");
+  [].forEach.call(fcRight,function(x) {
+    x.appendChild(switchMonth, null);
+  });
+  [].forEach.call(fcRight,function(x) {
+    x.appendChild(labelMonth, null);
+  });
+  [].forEach.call(fcRight,function(x) {
+    x.appendChild(switchDay, null);
+  });
+  [].forEach.call(fcRight,function(x) {
+    x.appendChild(labelDay, null);
+  });
+
+  //月・日の切り替え処理
+  $('input[name="switchMonthDay"]:radio').change( function() {
+    var radioVal = $(this).val();
+    var dailyView = document.getElementById('dailyView');
+    if(radioVal == 'month') {
+      var buttonMonth = document.getElementsByClassName('fc-month-button');
+      [].forEach.call(buttonMonth,function(x) {
+        x.click();
+      });
+      dailyView.style.display="none";
+    } else if(radioVal == 'day') {
+      var buttonDay = document.getElementsByClassName('fc-agendaDay-button');
+      [].forEach.call(buttonDay,function(x) {
+        x.click();
+      });
+      dailyView.style.display="block";
+    }
+  })
 }
